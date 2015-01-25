14550e795378ac565eca7132284baf697b795687
Mike Koss <mike@mckoss.com>
2015-01-24 21:11:13 -0800

    Create inner div for element

--- letter-tile/letter-tile.js
+++ letter-tile/letter-tile.js
@@ -1,7 +1,8 @@
 Polymer('letter-tile', {
   moveTo: function(x, y) {
     console.log("Moving to", x, y);
-    this.shadowRoot.host.style.left = x + 'px';
-    this.shadowRoot.host.style.top = y + 'px';
+    var tile = this.$.tile;
+    tile.style.left = x + 'px';
+    tile.style.top = y + 'px';
   }
 });
